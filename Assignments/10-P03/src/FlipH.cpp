b3c93031be740d625cc863d00d58e0f29df11444
cbridufleurand <cbdufleurand0109@my.msutexas.edu>
2026-05-12 21:27:10 -0500

    Implement horizontal flip in FlipH class
    Implemented the apply function to flip the grid horizontally by swapping pixels in each row.

--- Assignments/10-P03/src/FlipH.cpp
+++ Assignments/10-P03/src/FlipH.cpp
@@ -1,19 +1,24 @@
-#include "FlipH.h"
-#include <algorithm>    // std::swap
+##include "FlipH.h"
 
-std::string FlipH::name() const {
-    return "flipH";
-}
+#include <utility>
 
 void FlipH::apply(Grid& pixels) {
-    (void)pixels;  // remove this line when implementing
-    // TODO: iterate over every row and reverse it left-to-right.
-    //
-    // Hint: for each row, swap pixels at mirror positions:
-    //   for (auto& row : pixels) {
-    //       int width = row.size();
-    //       for (int col = 0; col < width / 2; ++col) {
-    //           std::swap(row[col], row[width - 1 - col]);
-    //       }
-    //   }
+
+    for (size_t row = 0; row < pixels.size(); ++row) {
+
+        size_t width = pixels[row].size();
+
+        for (size_t col = 0; col < width / 2; ++col) {
+
+            std::swap(
+                pixels[row][col],
+                pixels[row][width - 1 - col]
+            );
+        }
+    }
+}
+
+std::string FlipH::name() const {
+
+    return "flipH";
 }
